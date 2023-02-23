c85161b1ea5047018cb030c8d29b2dd56fab8c26
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2023-02-23 20:40:21 +0300

    Completed task

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,13 @@ class Song
 
   attr_accessor :name, :album, :id
 
+  @@all = []
+
   def initialize(name:, album:, id: nil)
     @id = id
     @name = name
     @album = album
+    @@all << self
   end
 
   def self.drop_table
@@ -49,4 +52,46 @@ class Song
     song.save
   end
 
+  #CReate a method that will convert the databse output into a Ruby object
+  def self.new_from_db(row)
+
+    #self.new is the equivalent to song.new This is not for records creation but reading data from SQLite and representing that temporarily in Ruby
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+
+  def self.all
+
+    sql=<<-SQL
+
+    SELECT * FROM  songs
+    SQL
+
+    #This will retrun an array of rows from the db that matches our query. All we have to do is iterate over each row and then use the self.map method to create a new ruby object for each row.
+    DB[:conn].execute(sql).map do |row|
+
+
+      self.new_from_db(row)
+    end
+  end
+
+  # Essentially we can also find the song by name
+  def self.find_by_name(name)
+    sql = <<-SQL
+
+    SELECT * FROM songs WHERE name = ? LIMIT 1
+
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+    # The end.first method simply grabs the first element of the returned array
+
+    #This will return a single row from the db that matches our query.
+  end
+
 end
+
+# We don't store ruby objects in the database and we do not get ruby objects from the database.
+# we store raw data describing a given ruby object in a table row 
+# When querying a database we write code that takes the data and turns it back into an instance of the appropriate class. It is the work of the programmer to translate the raw data that the database sends into a ruby object that has instances of that particular class. 
+
